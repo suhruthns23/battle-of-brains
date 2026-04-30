34169163ec7e29b1724f96a97a71eba17c8ebf9e
suhruthns23 <suhruthns@gmail.com>
2026-04-30 15:09:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
+function signup() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
 
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then(() => {
+      alert("Signup successful!");
+      window.location.href = "dashboard.html";
+    })
+    .catch(err => alert(err.message));
+}
+
+function login() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(() => {
+      window.location.href = "dashboard.html";
+    })
+    .catch(err => alert(err.message));
+}
