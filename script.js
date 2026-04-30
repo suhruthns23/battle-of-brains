60823b314398b116c90631109cf055cc6e65d3ba
suhruthns23 <suhruthns@gmail.com>
2026-04-30 14:50:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+function openTeam() {
+  document.getElementById("content").innerHTML = `
+    <h2>Select Playing XI</h2>
+    <p>(UI coming next 😤)</p>
+  `;
+}
 
+function viewLeaderboard() {
+  document.getElementById("content").innerHTML = `
+    <h2>Leaderboard</h2>
+    <p>(Data will come from sheet later)</p>
+  `;
+}
