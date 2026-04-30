cba6450d064d8d31a71c40d0e395db843ea1c934
suhruthns23 <suhruthns@gmail.com>
2026-04-30 15:07:15 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,13 @@
+// firebase.js
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBsQrz3OqZsm9rJyVlz8AkRQO3lL386cxM",
+  authDomain: "battle-of-brains-ca2cf.firebaseapp.com",
+  projectId: "battle-of-brains-ca2cf",
+  storageBucket: "battle-of-brains-ca2cf.appspot.com",
+  messagingSenderId: "932071888234",
+  appId: "1:932071888234:web:c7303370cbd5f04cec8113"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
